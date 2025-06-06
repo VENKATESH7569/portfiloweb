924b3279560b70b12e169ddc3402a5b8e9812c49
G Venkatesh <venkateshgadige916@gmail.com>
2025-06-06 16:44:50 +0530

    Updated portfolio with latest fixes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
-// vite.config.js
-export default {
-  root: './src'
-};
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  root: './src',
+  base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
+  plugins: [react()]
+});
